76959b14548b5603b5d77f61a77ac6115fd12d07
Ryan Dingle <Ryan.Dingle@asia.ing.com>
2019-09-09 23:54:59 +0800

    set to react build

---
+++ precache-manifest.316a72533cd866a0e1aed038de47a62b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80478768810c5c6ab8b76cb65d7e6957",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375f5b37aed61dec5a31",
+    "url": "/static/css/2.d5091aca.chunk.css"
+  },
+  {
+    "revision": "d2215227c7a5a98b7cac",
+    "url": "/static/css/main.723c410a.chunk.css"
+  },
+  {
+    "revision": "375f5b37aed61dec5a31",
+    "url": "/static/js/2.c1829767.chunk.js"
+  },
+  {
+    "revision": "d2215227c7a5a98b7cac",
+    "url": "/static/js/main.5ddfc8f7.chunk.js"
+  },
+  {
+    "revision": "78fba8b59b3b6cf7bdf9",
+    "url": "/static/js/runtime~main.0b19da71.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
